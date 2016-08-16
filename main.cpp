83a601da92a318e8a8204e12d18b219e4c4d21ab
student <student@c302.tnfsh.tn.edu.tw>
2016-08-16 10:30:17 +0800

    fguy1234

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cstdlib>
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) 
+{
+	
+	
+	std::cout<<"hello world"<<std::endl;
+	
+	return 0;
+}
