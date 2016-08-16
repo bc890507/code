091b4ef9e8c1e8e3f63069c5e3e695bde1d670c3
student <student@c302.tnfsh.tn.edu.tw>
2016-08-16 11:01:23 +0800

    x

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <cstdlib>
+using namespace std;
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char** argv) 
 {
-	
+	string a;
+	getline(cin,a);
 	
 	std::cout<<"hello world"<<std::endl;
 	
